19279079bc356b0bae3be58b802839c39e6da5e1
PJayasankarReddy <jayasankar.reddy@nexttrial.ai>
2022-03-16 18:13:50 +0530

    commit with adding 2 more lines

--- src/com/project/test/TestProject.java
+++ src/com/project/test/TestProject.java
@@ -5,7 +5,12 @@ public class TestProject {
 	public static void main(String[] args) {
 		
 System.out.println("jayasankarreddy");
-System.out.println("Reddy");	}
+System.out.println("Reddy");	
+System.out.println("NextTrial.ai");
+System.out.println("working");	
+
+	
+	}
 
 }
  
